745a72c12113da53408da11ee9f17af18d4a61b1
sispence <silospen@silospen.com>
2013-03-28 17:35:55 +0000

    element data only collects based on one sensor, graphs as grid background color because it's more pretty

--- src/com/silospen/BeerTempFrontEnd/static/assets/js/beerTemp/beerTemp.js
+++ src/com/silospen/BeerTempFrontEnd/static/assets/js/beerTemp/beerTemp.js
@@ -1,16 +1,38 @@
 (function ($, undef) {
 
-    function populateGraph(temperatureEvents) {
+    function startAndEndForGrid(elementDataSeries) {
+        var markings = [];
+        var currentStartTime;
+        for (var j = 0; j < elementDataSeries['data'].length; j++) {
+            if (elementDataSeries['data'][j][1] && j != elementDataSeries['data'].length - 1) {
+                if (!currentStartTime)currentStartTime = elementDataSeries['data'][j][0];
+            } else {
+                if (currentStartTime) {
+                    //noinspection JSUnusedAssignment
+                    markings.push({
+                        color: "#fee",
+                        xaxis: {
+                            from: currentStartTime,
+                            to: elementDataSeries['data'][j][0]
+                        }
+                    });
+                    currentStartTime = undef;
+                }
+            }
+        }
+        return markings;
+    }
 
+    function populateGraph(temperatureEvents) {
         var tempDataSeries = {};
         var elementDataSeries = {'yaxis': 2, 'label': 'Heater', 'data': []};
+        var firstSensor;
         for (var i = 0; i < temperatureEvents.length; i++) {
             var sensorId = temperatureEvents[i]['sensorId'];
-            if (tempDataSeries[sensorId] === undef) {
-                tempDataSeries[sensorId] = {'label': sensorId, 'data': []};
-            }
+            if (firstSensor === undef) firstSensor = sensorId;
+            if (tempDataSeries[sensorId] === undef) tempDataSeries[sensorId] = {'label': sensorId, 'data': []};
             tempDataSeries[sensorId]['data'].push([temperatureEvents[i]['time'] * 1000, temperatureEvents[i]['temp']]);
-            elementDataSeries['data'].push([temperatureEvents[i]['time'] * 1000, temperatureEvents[i]['active']]);
+            if (sensorId == firstSensor) elementDataSeries['data'].push([temperatureEvents[i]['time'] * 1000, temperatureEvents[i]['active']]);
         }
 
         var plottableData = [];
@@ -19,14 +41,16 @@
                 plottableData.push(tempDataSeries[series]);
             }
         }
-        plottableData.push(elementDataSeries);
 
         $.plot("#graph", plottableData, {
             xaxis: { mode: "time", timeformat: "%m/%d %H:%M" },
             yaxes: [
                 { position: 'left' },
                 { position: 'right', minTickSize: 1, max: 1 }
-            ]
+            ],
+            grid: {
+                markings: startAndEndForGrid(elementDataSeries)
+            }
         });
     }
 
@@ -38,6 +62,9 @@
             dataType: 'json',
             success: populateGraph
         });
+        setTimeout(function () {
+            getGraphDataAndPlot()
+        }, 20000);
     }
 
     getGraphDataAndPlot();
