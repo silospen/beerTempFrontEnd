79c9dfcd72b157006d2cf8800e68cd7b52190a4b
sispence <silospen@silospen.com>
2013-04-02 19:22:39 +0100

    Added control of the time to lookback over

--- src/com/silospen/BeerTempFrontEnd/static/assets/js/beerTemp/beerTemp.js
+++ src/com/silospen/BeerTempFrontEnd/static/assets/js/beerTemp/beerTemp.js
@@ -1,5 +1,6 @@
 (function ($, undef) {
 
+
     function startAndEndForGrid(elementDataSeries) {
         var markings = [];
         var currentStartTime = undef;
@@ -26,7 +27,10 @@
         var tempDataSeries = {};
         var elementData = [];
         var firstSensor;
+        var currentTime = Date.now()/1000;
+        var hoursToLookback = $('#lookback').val() * 24 * 3600;
         for (var i = 0; i < temperatureEvents.length; i++) {
+            if (temperatureEvents[i]['time'] < currentTime - hoursToLookback) continue;
             var sensorId = temperatureEvents[i]['sensorId'];
             if (firstSensor === undef) firstSensor = sensorId;
             if (tempDataSeries[sensorId] === undef) tempDataSeries[sensorId] = {'label': sensorId, 'data': []};
@@ -60,6 +64,19 @@
         setTimeout(function () {
             getGraphDataAndPlot()
         }, 20000);
+    }
+
+
+    function processForm(e) {
+        if (e.preventDefault) e.preventDefault();
+        getGraphDataAndPlot();
+    }
+
+    var form = document.getElementById('lookbackForm');
+    if (form.attachEvent) {
+        form.attachEvent("submit", processForm);
+    } else {
+        form.addEventListener("submit", processForm);
     }
 
     getGraphDataAndPlot();
